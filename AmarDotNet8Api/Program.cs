6de667e048d4da9d42e31f71c2f0442b995fb0e7
amarsharma45 <52012163+amarsharma45@users.noreply.github.com>
2025-07-28 13:34:25 +0530

    Enabled Cors

--- AmarDotNet8Api/Program.cs
+++ AmarDotNet8Api/Program.cs
@@ -2,12 +2,26 @@ using AmarDotNet8Api.Services;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularLocalhost", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddSingleton<ProductService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("AllowAngularLocalhost");
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
